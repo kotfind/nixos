320f24566f280be83f6ceab033b3aead68582bf2
kotfind <kotfind@yandex.ru>
2025-01-15 12:27:07 +0300

    Update utils

--- utils/default.nix
+++ utils/default.nix
@@ -1,29 +1,33 @@
 { pkgs, ... }:
-{
-    toHomeFiles =
-        let 
-            listFilesRecursiveHelper = root: dir: 
+rec {
+    # Lists all regular files in current directory in format.
+    # Paths in outputs are relative to current directory.
+    # Note: `root` is usually `./.`
+    listFilesRecursive =
+        let
+            helper = root: dir: 
                 (pkgs.lib.mapAttrsToList
                     (name: type:
                         let
-                            fullname = "${dir}/${name}";
+                            sep = if dir == "" then "" else "/";
+                            fullname = "${dir}${sep}${name}";
                         in
                         if type == "regular"
                             then fullname
-                            else listFilesRecursiveHelper root fullname
+                            else helper root fullname
                     )
                     (builtins.readDir "${root}/${dir}")
                 );
+        in root: dir: pkgs.lib.flatten (helper root dir);
 
-            listFilesRecursive = root: pkgs.lib.flatten (listFilesRecursiveHelper root ".");
+    toHomeFiles = dir: files: builtins.listToAttrs
+        (builtins.map
+            (path: {
+                name = path;
+                value = "${dir}/${path}";
+            })
+            files
+        );
 
-            toHomeFiles = dir: builtins.listToAttrs
-                (builtins.map
-                    (path: {
-                        name = path;
-                        value = "${dir}/${path}";
-                    })
-                    (listFilesRecursive dir)
-                );
-        in toHomeFiles;
+    toHomeFilesRec = root: dir: toHomeFiles dir (listFilesRecursive root dir);
 }
