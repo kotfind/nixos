3054a3c506d369f4f0a36a954a46a527b4636eda
kotfind <kotfind@yandex.ru>
2025-01-15 12:29:35 +0300

    Change alacritty config

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,8 +1,7 @@
 { ... }: {
     programs.alacritty.enable = true;
 
-    home.file.".config/alacritty" = {
-        source = ./.config/alacritty;
-        recursive = true;
-    };
+    home.file.".config/alacritty/alacritty.toml".source = ./.config/alacritty/alacritty.toml;
+    home.file.".config/alacritty/dark_theme.toml".source = ./.config/alacritty/dark_theme.toml;
+    home.file.".config/alacritty/light_theme.toml".source = ./.config/alacritty/light_theme.toml;
 }
