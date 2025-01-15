964e063cfbfa8544013026e257acf40dc0d081e9
kotfind <kotfind@yandex.ru>
2025-01-16 02:58:42 +0300

    Fix pass directory

--- home/pass.nix
+++ home/pass.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ cfg, ... }:
 {
     programs.password-store = {
         enable = true;
+        settings = {
+            # it's not the default for whatever reason
+            PASSWORD_STORE_DIR = "/home/${cfg.username}/.password-store";
+        };
     };
 
     # TODO: auto clone
