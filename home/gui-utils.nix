963b1d2050b9ae00349824fea60d3551ecd63182
kotfind <kotfind@yandex.ru>
2025-01-22 15:04:05 +0300

    Fcitx5, telegram-desktop workaround

--- home/gui-utils.nix
+++ home/gui-utils.nix
@@ -5,6 +5,10 @@
         vlc-bittorrent # TODO: won't work
         transmission_4-qt
         sxiv
+
+        # Run this to make fcitx5 to work in telegram
+        # sudo dbus-update-activation-environment --all
+        # source: https://github.com/telegramdesktop/tdesktop/issues/26891
         telegram-desktop
     ];
 
