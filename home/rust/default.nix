4b43890df8362ec631878f6199f85ee3f8af994e
kotfind <kotfind@yandex.ru>
2025-01-22 16:36:54 +0300

    rust: fix config

--- home/rust/default.nix
+++ home/rust/default.nix
@@ -1,5 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
-    home.file.".cargo/config.toml".source = ./.cargo/config.toml;
-    home.file.".config/ra-multiplex/config.toml".source = ./.config/ra-multiplex/config.toml;
+    home.file.".cargo/config.toml".text = ''
+        [target.x86_64-unknown-linux-gnu]
+        linker = "${pkgs.clang}/bin/clang"
+        rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold"]
+    '';
+
+    home.file.".config/ra-multiplex/config.toml".text = ''
+        instance_timeout = 300
+    '';
 }
