b460700bef082079a883b0ac907ff3c496639e7e
kotfind <kotfind@yandex.ru>
2025-01-22 22:38:57 +0300

    Add gh_auth token

--- home/secrets/default.nix
+++ home/secrets/default.nix
@@ -5,7 +5,9 @@
             sshKeyPaths = [];
             keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
         };
+
         defaultSopsFile = ./default.yaml;
+
         secrets =
             {
                 kotfindPC-ip-address = {};
@@ -13,6 +15,29 @@
             (if cfg.fullname == "kotfind@kotfindPC" then {
                 "kotfind@kotfindPC/ssh/id_rsa".path = "/home/kotfind/.ssh/id_rsa";
                 "kotfind@kotfindPC/ssh/id_rsa.pub".path = "/home/kotfind/.ssh/id_rsa.pub";
+            } else {}) //
+            (if cfg.username == "kotfind" then {
+                "kotfind/gh/oauth_token" = {};
+            } else {});
+
+        templates =
+            {
+            } //
+            (if cfg.username == "kotfind" then {
+                "gh_hosts" = {
+                    content = let
+                        oauth_token = "${config.sops.placeholder."kotfind/gh/oauth_token"}";
+                    in ''
+                        github.com:
+                            users:
+                                kotfind:
+                                    oauth_token: ${oauth_token}
+                            git_protocol: ssh
+                            user: kotfind
+                            oauth_token: ${oauth_token}
+                    '';
+                    path = "/home/kotfind/.config/gh/hosts.yml";
+                };
             } else {});
     };
 
