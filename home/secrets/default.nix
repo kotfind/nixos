1d1860852d9c338cdd742d5fa3da4adaf0b6502c
kotfind <kotfind@yandex.ru>
2025-01-22 22:17:21 +0300

    Ssh keys

--- home/secrets/default.nix
+++ home/secrets/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ pkgs, config, cfg, ... }:
 {
     sops = {
         age = {
@@ -6,9 +6,14 @@
             keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
         };
         defaultSopsFile = ./default.yaml;
-        secrets = {
-            kotfindPC-ip-address = {};
-        };
+        secrets =
+            {
+                kotfindPC-ip-address = {};
+            } //
+            (if cfg.fullname == "kotfind@kotfindPC" then {
+                "kotfind@kotfindPC/ssh/id_rsa".path = "/home/kotfind/.ssh/id_rsa";
+                "kotfind@kotfindPC/ssh/id_rsa.pub".path = "/home/kotfind/.ssh/id_rsa.pub";
+            } else {});
     };
 
     home.packages = with pkgs; [
