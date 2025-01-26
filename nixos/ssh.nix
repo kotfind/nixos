077d9a8f07ad4a4aa0f69bb96394211ff5993201
kotfind <kotfind@yandex.ru>
2025-01-26 17:40:57 +0300

    ssh: enable X11Forwarding

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -3,6 +3,7 @@
     services.openssh = {
         enable = true;
         settings = {
+            X11Forwarding = true;
             PasswordAuthentication = false;
             KbdInteractiveAuthentication = false;
         };
