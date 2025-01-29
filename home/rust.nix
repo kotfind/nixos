bb02c63f30dd3a9cda0185911af7f25d6d1ed039
kotfind <kotfind@yandex.ru>
2025-01-29 10:55:52 +0300

    rust: add sccache

--- home/rust.nix
+++ home/rust.nix
@@ -4,6 +4,9 @@
         [target.x86_64-unknown-linux-gnu]
         linker = "${pkgs.clang}/bin/clang"
         rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold"]
+
+        [build]
+        rustc-wrapper = "${pkgs.sccache}/bin/sccache"
     '';
 
     home.file.".config/ra-multiplex/config.toml".text = ''
