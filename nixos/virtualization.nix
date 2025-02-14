b28e4e5a37bc228a3fa29fc0a8eb6de4e0ba3451
kotfind <kotfind@yandex.ru>
2025-02-14 23:43:11 +0300

    Add podman overlay

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -1,15 +1,29 @@
 { pkgs, ... }:
 {
-    environment.systemPackages = with pkgs; [
-        podman-compose
-    ];
-
     virtualisation = {
         containers.enable = true;
+        oci-containers.backend = "podman";
+
         podman = {
             enable = true;
             dockerCompat = true;
             defaultNetwork.settings.dns_enabled = true;
         };
     };
+
+    environment.systemPackages = with pkgs; [
+        podman-compose
+    ];
+
+    # Fix podman
+    nixpkgs.overlays = [ (final: prev: {
+        podman = prev.podman.override {
+            extraPackages = [
+                # https://github.com/NixOS/nixpkgs/issues/138423#issuecomment-1609849179
+                "/run/wrappers/"
+
+                pkgs.shadow
+            ];
+        };
+    }) ];
 }
