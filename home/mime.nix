3cc0afd4fca64c7aa0d79a92dbb0dd437f361062
kotfind <kotfind@yandex.ru>
2025-02-19 13:17:05 +0300

    Fix mimeapps.list

--- home/mime.nix
+++ home/mime.nix
@@ -1,31 +1,23 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 let
-    desktop' = pkgName: desktopFileName: "${pkgs.${pkgName}}/share/applications/${desktopFileName}";
-    desktop = pkgName: desktop' pkgName "${pkgName}.desktop";
+    telegram = "org.telegram.desktop.desktop";
+    zathura = "org.pwmt.zathura-pdf-mupdf.desktop";
+    sxiv = "sxiv.desktop";
+    firefox = "firefox.desktop";
 in
 {
-    # FIXME: don't seem to work
     xdg.mimeApps = (with config.cfgLib; enableFor users.kotfind) {
         enable = true;
-        defaultApplications =
-            {
-                "x-scheme-handler/tonsite" = desktop' "telegram-desktop" "org.telegram.desktop.desktop";
-                "application/pdf" = desktop' "zathura" "org.pwmt.zathura-pdf-mupdf.desktop";
-            }
-            // (let
-                sxiv = desktop "sxiv";
-            in {
-                "image/heif" = sxiv;
-                "image/gif"  = sxiv;
-                "image/png"  = sxiv;
-                "image/jpeg" = sxiv;
-            })
-            // (let
-                firefox = desktop "firefox";
-            in { 
-                "x-scheme-handler/http"  = firefox;
-                "x-scheme-handler/https" = firefox;
-                "text/html"              = firefox;
-            });
+        defaultApplications = {
+            "x-scheme-handler/tonsite" = telegram;
+            "application/pdf" = zathura;
+            "image/heif" = sxiv;
+            "image/gif" = sxiv;
+            "image/png" = sxiv;
+            "image/jpeg" = sxiv;
+            "x-scheme-handler/http" = firefox;
+            "x-scheme-handler/https" = firefox;
+            "text/html" = firefox;
+        };
     };
 }
