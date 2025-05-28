a1c9c8dd3000623024b2ac9bde15b9d3d6d875e9
kotfind <kotfind@yandex.ru>
2025-05-28 20:41:17 +0300

    nvim: sync theme with alacritty

--- home/nvim/lua/looks.lua
+++ home/nvim/lua/looks.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local function setup_theme()
-    require 'cyberdream'.setup {
+    require('cyberdream').setup {
         italic_comments = true,
         borderless_pickers = true,
         variant = 'auto',
@@ -13,7 +13,7 @@ local function setup_theme()
                 ['@note.comment'] = { fg = colors.cyan, bold = true },
 
                 -- for listchars
-                Whitespace = { fg = colors.white, bg = colors.red }
+                Whitespace = { fg = colors.white, bg = colors.red },
             }
         end,
     }
@@ -21,8 +21,46 @@ local function setup_theme()
     vim.cmd [[ colorscheme cyberdream ]]
 end
 
+--- synchronizes vim.opt.background with system,
+--- based of ~/.config/alacritty/active-theme.toml file
+local function setup_sync_background()
+    local theme_symlink = vim.fn.expand '~/.config/alacritty/active-theme.toml'
+
+    local timer = vim.uv.new_timer()
+    local timer_delay = 100 -- ms
+
+    timer:start(
+        0,
+        timer_delay,
+        vim.schedule_wrap(function()
+            local theme_file = vim.uv.fs_readlink(theme_symlink)
+            if theme_file == nil then
+                vim.notify(
+                    'failed to read theme symlink' .. theme_symlink,
+                    vim.log.levels.ERROR
+                )
+                timer:stop()
+                return
+            end
+
+            if string.find(theme_file, 'light', 1, true) then
+                vim.opt.background = 'light'
+            elseif string.find(theme_file, 'dark', 1, true) then
+                vim.opt.background = 'dark'
+            else
+                vim.notify(
+                    'failed to determine theme type of file: ' .. theme_file,
+                    vim.log.levels.ERROR
+                )
+                timer:stop()
+                return
+            end
+        end)
+    )
+end
+
 local function setup_lualine()
-  require 'lualine'.setup {
+    require('lualine').setup {
         options = {
             theme = require 'lualine.themes.cyberdream',
             component_separators = {
@@ -102,10 +140,11 @@ function M.setup()
     setup_lualine()
     setup_notify()
     setup_noice()
+    setup_sync_background()
 
-    require 'smear_cursor'.setup {}
-    require 'neoscroll'.setup {}
-    require 'nvim-web-devicons'.setup {}
+    require('smear_cursor').setup {}
+    require('neoscroll').setup {}
+    require('nvim-web-devicons').setup {}
 end
 
 return M
