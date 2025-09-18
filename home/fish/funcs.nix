69759741c8f0c7a0ddd68479d38bf8f0f1efdb68
kotfind <kotfind@yandex.ru>
2025-09-18 10:12:01 +0300

    notify-on-finish: add --disable option

--- home/fish/funcs.nix
+++ home/fish/funcs.nix
@@ -33,24 +33,47 @@ in {
         $argv[1..-1] &> /dev/null & disown
       '';
 
+    _notify-on-finish_notify =
+      # fish
+      ''
+        set -l focused_window_pid (${xdotool} getwindowfocus getwindowpid)
+        if [ -z "$focused_window_pid" ]
+          return
+        end
+
+        set -l this_pid $fish_pid
+        set -l focused_pids (
+          string split (
+            ${pstree} -p $focused_window_pid | ${awk} '{ print $2; }'
+          )
+        )
+
+        if not contains $this_pid $focused_pids
+          ${notify-send} \
+            -a 'fish-command-executed' \
+            'Command executed' \
+            $argv[1]
+        end
+      '';
+
     notify-on-finish = {
       onEvent = "fish_postexec";
       body =
         # fish
         ''
-          set -l focused_window_pid (${xdotool} getwindowfocus getwindowpid)
-          set -l this_pid $fish_pid
-          set -l focused_pids (
-            string split (
-              ${pstree} -p $focused_window_pid | ${awk} '{ print $2; }'
-            )
-          )
+          if [ (count $argv) != 1 ]
+            echo 'failed to parse args' 1>&2
+            return 1
+          end
 
-          if not contains $this_pid $focused_pids
-            ${notify-send} \
-              -a 'fish-command-executed' \
-              'Command executed' \
-              $argv[1]
+          if [ $argv[1] = '--disable' ]; or [ $argv[1] = '-d' ]
+            set -g NO_NOTIFY_ON_FINISH
+            set -x NO_NOTIFY_ON_FINISH
+            return
+          end
+
+          if not set -q NO_NOTIFY_ON_FINISH
+            _notify-on-finish_notify $argv[1]
           end
         '';
     };
@@ -62,7 +85,11 @@ in {
       # manually load an --on-event function
       source ~/.config/fish/functions/notify-on-finish.fish
 
-      # completion for start
+      # complete for `start`
       complete -c start -xa "(__fish_complete_subcommand)"
+
+      # complete for `notify-on-finish`
+      complete -c notify-on-finish -f
+      complete -c notify-on-finish -s d -l disable -d 'disable on-finish notifications'
     '';
 }
