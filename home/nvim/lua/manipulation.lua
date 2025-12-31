b392aad96cdeb896f908b8423c6d9afc8d7cd3e6
kotfind <kotfind@yandex.ru>
2025-12-31 17:00:55 +0300

    nvim: disable single quote autocomplete

--- home/nvim/lua/manipulation.lua
+++ home/nvim/lua/manipulation.lua
@@ -1,17 +1,19 @@
 --
--- This file sets up some text manipulation logics, like 
+-- This file sets up some text manipulation logics, like
 -- commenting and surrounding with braces, hence the name.
 --
 
 local M = {}
 
 local function setup_comment()
-    require 'ts_context_commentstring'.setup {
+    require('ts_context_commentstring').setup {
         enable_autocmd = false,
     }
 
-    require 'Comment'.setup {
-        pre_hook = require 'ts_context_commentstring.integrations.comment_nvim'.create_pre_hook(),
+    require('Comment').setup {
+        pre_hook = require(
+            'ts_context_commentstring.integrations.comment_nvim'
+        ).create_pre_hook(),
         toggler = {
             line = '<leader>cc',
             block = '<leader>bc',
@@ -28,15 +30,21 @@ local function setup_comment()
     }
 end
 
-local function setup_braces()
-    require 'nvim-surround'.setup {}
-    require 'autoclose'.setup {}
-    require 'nvim-ts-autotag'.setup {}
+local function setup_autoclose()
+    require('autoclose').setup {
+        keys = {
+            -- disabling single quote as it's lifetime specifier in rust
+            ["'"] = { enabled_filetypes = {} },
+        },
+    }
 end
 
 function M.setup()
     setup_comment()
-    setup_braces()
+    setup_autoclose()
+
+    require('nvim-surround').setup {}
+    require('nvim-ts-autotag').setup {}
 end
 
 return M
