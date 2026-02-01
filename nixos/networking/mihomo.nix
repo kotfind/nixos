00b676095615e5f1a35626083340207600f72f14
kotfind <kotfind@yandex.ru>
2026-02-01 14:11:18 +0300

    mihomo with sops

--- nixos/networking/mihomo.nix
+++ nixos/networking/mihomo.nix
@@ -1,8 +1,16 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (builtins) readFile listToAttrs;
+  inherit (config) sops;
+  inherit (lib.attrsets) nameValuePair mapAttrsRecursive mapAttrsToListRecursive;
+
   mihomoConfig = {
     mode = "global";
-    # FIXME: actual secret
-    secret = "SECRET";
+    secret = ph.secret;
 
     allow-lan = false;
     external-controller = "localhost:4343"; # FIXME: https
@@ -34,23 +42,32 @@
     ];
   };
 
-  dummyUrl = "http://www.gstatic.com/generate_204";
-
   providersConfig = {
-    # FIXME: actual url
-    http-1-provider = httpProvider (mins 30) dummyUrl {};
-
-    # FIXME: actual url
-    http-2-provider = httpProvider (mins 5) dummyUrl {};
+    ${ph.provider-1-name} = httpProvider (mins 30) ph.provider-1-url {};
+    ${ph.provider-2-name} = httpProvider (mins 5) ph.provider-2-url {};
   };
 
   groupsConfig = [
-    (urlTestGroup "url-test-1-group" {use = ["http-1-provider"];})
-    (urlTestGroup "url-test-2-group" {use = ["http-2-provider"];})
+    (urlTestGroup ph.group-1-name {use = [ph.provider-1-name];})
+    (urlTestGroup ph.group-2-name {use = [ph.provider-2-name];})
     (selectGroup "manual-group" {include-all = true;})
   ];
 
+  secrets = {
+    secret = "common/mihomo/secret";
+
+    provider-1-url = "common/mihomo/providers/1/url";
+    provider-1-name = "common/mihomo/providers/1/name";
+
+    provider-2-url = "common/mihomo/providers/2/url";
+    provider-2-name = "common/mihomo/providers/2/name";
+
+    group-1-name = "common/mihomo/groups/1/name";
+    group-2-name = "common/mihomo/groups/2/name";
+  };
+
   # -------------------- Group Helpers --------------------
+
   baseGroup = type: name: extra:
     {
       inherit name type;
@@ -75,6 +92,16 @@
   httpProvider = interval: url: extra:
     baseProvider "http" interval ({inherit url;} // extra);
 
+  # -------------------- Sops Helpers --------------------
+
+  ph = mapAttrsRecursive (_path: qual: sops.placeholder.${qual}) secrets;
+
+  secretDefs = listToAttrs (
+    map
+    (qual: nameValuePair qual {})
+    (mapAttrsToListRecursive (_path: qual: qual) secrets)
+  );
+
   # -------------------- Other Helpers --------------------
 
   toYaml = (pkgs.formats.yaml {}).generate;
@@ -84,11 +111,21 @@
   # -------------------- Other --------------------
 
   sampleUrl = "https://youtube.com";
+
+  rawConfigFile = toYaml "mihomo-config-raw.yml" mihomoConfig;
 in {
   services.mihomo = {
     enable = true;
     tunMode = true;
     webui = pkgs.metacubexd;
-    configFile = toYaml "mihomo-config-raw.yml" mihomoConfig;
+    configFile = sops.templates."mihomo-config.yml".path;
+  };
+
+  sops = {
+    secrets = secretDefs;
+    templates."mihomo-config.yml" = {
+      content = readFile rawConfigFile;
+      restartUnits = ["mihomo.service"];
+    };
   };
 }
