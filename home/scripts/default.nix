c2c14534bb5d28b2abb58b9fe840dc1e7d3ce6f6
kotfind <kotfind@yandex.ru>
2026-04-12 21:53:49 +0300

    add dir2prompt script

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,5 +1,9 @@
-{...}: {
-  imports = [
-    ./nolink.nix
+{pkgs, ...}: let
+  inherit (pkgs) writeShellScriptBin;
+  inherit (pkgs.writers) writePython3Bin;
+in {
+  home.packages = [
+    (writeShellScriptBin "nolink" ./nolink.sh)
+    (writePython3Bin "dir2prompt" {flakeIgnore = ["E501"];} ./dir2prompt.py)
   ];
 }
---
+++ home/scripts/dir2prompt.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+
+def is_path_ignored(path: Path):
+    exclude_names = [".git", ".direnv", "build", "target", ".gitignore"]
+
+    if path.name in exclude_names:
+        return True
+
+    if "cache" in path.name:
+        return True
+
+    if "lock" in path.name:
+        return True
+
+    return False
+
+
+def print_tree(path: Path, depth: int = 0):
+    indent = " " * 4 * depth
+    for entry_path in sorted(path.iterdir()):
+        if is_path_ignored(entry_path):
+            continue
+
+        print(f"{indent}{entry_path.name}")
+
+        if entry_path.is_dir():
+            print_tree(entry_path, depth + 1)
+
+
+def print_file_contents_rec(path: Path, base_path: Path):
+    for entry_path in sorted(path.iterdir()):
+        if is_path_ignored(entry_path):
+            continue
+
+        if entry_path.is_file():
+            rel_path = entry_path.relative_to(base_path)
+            print(f"The contents of `{rel_path}`:")
+
+            lines = entry_path.read_text(errors="backslashreplace").splitlines()
+            lines = [line for line in lines]
+            contents = "\n".join(lines)
+
+            print("```")
+            print(contents)
+            print("```")
+            print()
+
+        if entry_path.is_dir():
+            print_file_contents_rec(entry_path, base_path)
+
+
+if __name__ == "__main__":
+    print("I have the following file structure:")
+    print("```")
+    print_tree(Path.cwd(), 0)
+    print("```")
+    print()
+
+    print("File contents are the following:")
+    print_file_contents_rec(Path.cwd(), Path.cwd())
