836f3aadb0424b6f0661ab8e65e9ae567e67e2de
kotfind <kotfind@yandex.ru>
2026-04-17 21:04:34 +0300

    fix ble.sh up/down bindings

--- home/bash/blesh/bind.nix
+++ home/bash/blesh/bind.nix
@@ -4,7 +4,7 @@
     ble-bind -f 'M-e' 'edit-and-execute-command'
     ble-bind -f 'C-RET' 'accept-line'
 
-    ble-bind -f 'up'   'history-search-backward hide-status:action=accept:point=end:immediate-accept'
-    ble-bind -f 'down' 'history-search-forward  hide-status:action=accept:point=end:immediate-accept'
+    ble-bind -f 'up'   'history-search-backward hide-status:action=load:point=end:immediate-accept'
+    ble-bind -f 'down' 'history-search-forward  hide-status:action=load:point=end:immediate-accept'
   '';
 }
