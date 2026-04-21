236db10bd866db24571d925aa9472de4a5800c98
kotfind <kotfind@yandex.ru>
2026-04-21 12:20:15 +0300

    configure beets

--- home/beet.nix
+++ home/beet.nix
@@ -1,17 +1,64 @@
 {
   pkgs,
+  lib,
   config,
   ...
 }: let
-  inherit (config.cfgLib) enableFor hosts;
+  inherit (config.cfgLib) matchFor hosts;
+  inherit (lib) genAttrs const;
 
-  kotfindOnPc = hosts.pc.users.kotfind;
+  builtinPlugins = [
+    "chroma"
+    "deezer"
+    "edit"
+    "fetchart"
+    "fromfilename"
+    "lastgenre"
+    "lyrics"
+    "musicbrainz"
+    "duplicates"
+    "fuzzy"
+    "info"
+    "missing"
+    "web"
+    "whatlastgenre"
+  ];
 in {
-  home.packages = enableFor kotfindOnPc (with pkgs; [
-    beets
-  ]);
+  programs.beets = {
+    enable = matchFor hosts.pc.users.kotfind;
 
-  home.sessionVariables = enableFor kotfindOnPc {
-    BEETSDIR = "/hdd/data/music/beet";
+    package = lib.pipe pkgs.python3.pkgs.beets [
+      (it:
+        it.overrideAttrs {
+          doCheck = false;
+          dontUsePytestCheck = true;
+        })
+      (it:
+        it.override {
+          pluginOverrides =
+            genAttrs builtinPlugins
+            <| const {enable = true;};
+        })
+    ];
+
+    settings = {
+      directory = "/hdd/data/music/songs";
+      library = "/hdd/data/music/beet/beet.db";
+      path = "relative";
+
+      terminal-encoding = "utf-8";
+
+      threaded = true;
+
+      ui.color = true;
+
+      import = {
+        write = true;
+        copy = false;
+        move = true;
+      };
+
+      plugins = builtinPlugins;
+    };
   };
 }
