722b90ddadf73b6f09eb08813f52e2b0a473ad4f
kotfind <kotfind@yandex.ru>
2026-07-04 14:13:54 +0300

    fix caches

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -12,7 +12,7 @@
 
   nix.settings = {
     substituters = [
-      "https://nix-community.cachix.org?=100"
+      "https://nix-community.cachix.org?priority=100"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
