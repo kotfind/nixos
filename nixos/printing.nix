f0659f2793ce6d11a23a61261beb7915cb970592
kotfind <kotfind@yandex.ru>
2026-08-11 17:12:10 +0300

    restrict printers to pc

--- nixos/printing.nix
+++ nixos/printing.nix
@@ -1,4 +1,6 @@
-{pkgs, ...}: let
+{pkgs, config, ...}: let
+  inherit (config.cfgLib) enableFor hosts;
+
   printerName = "HP_LaserJet_M1120_MFP";
 in {
   services.printing = {
@@ -12,16 +14,18 @@ in {
     ];
   };
 
-  hardware.printers.ensureDefaultPrinter = printerName;
-  hardware.printers.ensurePrinters = [
-    {
-      name = printerName;
-      deviceUri = "usb://HP/LaserJet%20M1120%20MFP?serial=MF28T32&interface=1";
-      model = "HP/hp-laserjet_m1120_mfp.ppd.gz";
-      ppdOptions = {
-        PageSize = "A4";
-        Double-Sided = "None";
-      };
-    }
-  ];
+  hardware.printers = enableFor hosts.pc {
+    ensureDefaultPrinter = printerName;
+    ensurePrinters = [
+      {
+        name = printerName;
+        deviceUri = "usb://HP/LaserJet%20M1120%20MFP?serial=MF28T32&interface=1";
+        model = "HP/hp-laserjet_m1120_mfp.ppd.gz";
+        ppdOptions = {
+          PageSize = "A4";
+          Double-Sided = "None";
+        };
+      }
+    ];
+  };
 }
