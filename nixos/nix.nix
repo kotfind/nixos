23b4dff70e498bd107aabdddee4d54d11969ca65
kotfind <kotfind@yandex.ru>
2026-08-17 18:54:28 +0300

    auto collect garbage

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -20,4 +20,10 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
+
+  nix.gc = {
+    automatic = true;
+    dates = ["16:00"];
+    options = "--delete-older-than 30d";
+  };
 }
