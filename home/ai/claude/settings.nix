3c69bf6ca6a9f6a8db92fff90cabb87dbcf016e4
kotfind <kotfind@yandex.ru>
2026-08-29 21:33:20 +0300

    claude: disable mouse

--- home/ai/claude/settings.nix
+++ home/ai/claude/settings.nix
@@ -1,3 +1,7 @@
 {...}: {
-  programs.claude-code.settings.alwaysThinkingEnabled = false;
+  programs.claude-code.settings = {
+    alwaysThinkingEnabled = false;
+
+    env.CLAUDE_CODE_DISABLE_MOUSE = "1";
+  };
 }
