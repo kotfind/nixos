f15fb13a482375f4f32448c36ffc98d0086c5c45
kotfind <kotfind@yandex.ru>
2026-08-29 21:35:44 +0300

    claude: lower thinking

--- home/ai/claude/settings.nix
+++ home/ai/claude/settings.nix
@@ -3,5 +3,7 @@
     alwaysThinkingEnabled = false;
 
     env.CLAUDE_CODE_DISABLE_MOUSE = "1";
+    env.CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
+    env.CLAUDE_CODE_EFFORT_LEVEL = "low";
   };
 }
