a833c99a88f97a2316b9631e60d1efa564d4e851
kotfind <kotfind@yandex.ru>
2026-09-07 11:30:31 +0300

    fix nvidia resolution

--- system/gpu.nix
+++ system/gpu.nix
@@ -4,4 +4,11 @@ in {
   hardware.graphics.enable = trueFor hosts.pc;
   services.xserver.videoDrivers = mkFor hosts.pc ["nvidia"];
   hardware.nvidia.open = trueFor hosts.pc;
+
+  services.xserver.config = mkFor hosts.pc ''
+    Section "Screen"
+      Identifier "Screen-nvidia[0]"
+      Option "metamodes" "HDMI-0: 1920x1080_60 +0+0"
+    EndSection
+  '';
 }
