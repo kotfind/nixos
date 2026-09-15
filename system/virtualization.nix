198444b05b41198f5eadeb3df9ff6073b267cb83
kotfind <kotfind@yandex.ru>
2026-09-16 00:40:26 +0300

    virt-manager

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -1,5 +1,14 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  inherit (config.hostlib) hosts trueFor mkFor;
+in {
   virtualisation = {
+    libvirtd.enable = trueFor hosts.pc;
+    libvirtd.qemu.vhostUserPackages = mkFor hosts.pc (with pkgs; [virtiofsd]);
     containers.enable = true;
     oci-containers.backend = "podman";
 
@@ -14,9 +23,18 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    podman-compose
-  ];
+  environment.systemPackages = with pkgs;
+    lib.mkMerge [
+      [
+        podman-compose
+      ]
+      (mkFor hosts.pc [
+        qemu_kvm
+        OVMF
+      ])
+    ];
+
+  programs.virt-manager.enable = trueFor hosts.pc;
 
   environment.sessionVariables = {
     PODMAN_COMPOSE_WARNING_LOGS = "false";
